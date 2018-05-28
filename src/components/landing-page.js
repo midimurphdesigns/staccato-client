f9689d0be0f6b4e6f73a74ef5d9fdf11df29ce08
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-28 09:22:49 -0700

    initial commit

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
 import LoginForm from './login-form';
 
@@ -12,9 +12,33 @@ export function LandingPage(props) {
 
     return (
         <div className="home">
-            <h2>Welcome to Foo App</h2>
-            <LoginForm />
-            <Link to="/register">Register</Link>
+            <div className="row">
+                <section className="title-section">
+                    <h3>Welcome to...</h3>
+                    <h1>Staccato</h1>
+                    <h3>A music theory learning app</h3>
+                </section>
+                <section className="login">
+                    <LoginForm />
+                    <label>Need to create an account?</label>
+                    <Link to="/register">Register</Link>
+                </section>
+            </div>
+
+            <div className="row">
+                <section className="info-block">
+                    <label>Expand your music theory through...</label>
+                    <h3>Space repetition!</h3>
+                </section>
+                <section className="info-block">
+                    <label>Keep track of your...</label>
+                    <h3>Progress!</h3>
+                </section>
+                <section className="info-block">
+                    <label>Let our algorithm suggest to you...</label>
+                    <h3>Chords, scales, and terminology!</h3>
+                </section>
+            </div>
         </div>
     );
 }
