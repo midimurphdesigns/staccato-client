0314696b98b00fe58ec78b273551b0057ab3646d
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-29 08:06:03 -0700

    landing page style changes

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -13,25 +13,25 @@ export function LandingPage(props) {
 
     return (
         <div className="home">
-            <div className="welcome-login-container row col-12">
+            <div className="welcome-section row col-12">
                 <section className="title-section col-6">
                     <h3>Welcome to...</h3>
                     <h1>Staccato</h1>
                     <h3>A music theory learning app</h3>
                 </section>
-                <section className="login col-6">
-                    <LoginForm />
+                <section className="login-section col-6">
+                    <LoginForm className="login"/>
                     <label>Need to create an account?</label>
                     <Link to="/register">Register</Link>
                 </section>
             </div>
 
-            <div className="row col-12">
+            <div className="info-section row col-12">
                 <section className="info-block col-3">
                     <label>Expand your music theory through...</label>
                     <h3>Space repetition!</h3>
                 </section>
-                <section className="info-block col-3">
+                <section className="info-block center-info-block col-3">
                     <label>Keep track of your...</label>
                     <h3>Progress!</h3>
                 </section>
